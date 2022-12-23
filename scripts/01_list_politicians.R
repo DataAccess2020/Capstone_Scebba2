4d4e36bc2369ee9625a9435c3528dbed4fdeabd9
saraalicescebba <saraalice.scebba@studenti.unimi.it>
2022-12-23 10:06:31 +0100

    list of politicians

---
+++ scripts/01_list_politicians.R
@@ -0,0 +1,23 @@
+right_lomb = lists_members(
+  list_id = "1604889112456138769",
+  slug = NULL, 
+  owner_user = NULL,
+  n = 26,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
+)
+
+left_lomb = lists_members(
+  list_id = "1605175421473112064",
+  slug = NULL, 
+  owner_user = NULL,
+  n = 23,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
+)
